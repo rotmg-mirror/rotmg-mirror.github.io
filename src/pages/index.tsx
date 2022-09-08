1ecde19648b22fbcf24b02599aba6fc1df40b1e5
amy <ctrlalternativedelete@gmail.com>
2022-09-08 10:49:36 +1000

    fixed up link again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
           <TechnologyCard
             name="DPS Calculator"
             description="A tool that calculates your DPS in game on average."
-            documentation="https://jy126.github.io/rotmg-dps-calculator/"
+            documentation="https://jy1263.github.io/rotmg-dps-calculator/"
           />
           <TechnologyCard
             name="Skin Viewer"
